a0562d49452fe92ff7f1e3ce53fb014c02d0d171
anglersoup <webberthespider48@gmail.com>
2024-02-04 01:35:20 -0500

    did awesome stuff and everything broke

---
+++ src/main/java/angler/tutorialmod/TutorialMod.java
@@ -0,0 +1,18 @@
+package angler.tutorialmod;
+
+import item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmod");
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+
+	}
+}
